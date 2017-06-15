66ac54725a154993a583953db6262ccc66adf3a0
Pascal <pascal.gruen@gmail.com>
2017-06-15 19:17:42 +0200

    Maven: Parse information from parent tag

--- Makefile
+++ Makefile
@@ -5,4 +5,5 @@ install:
 	cp mvn-download-artifact.sh ~/bin/mvn-download-artifact
 	cp mvn-parse-pom.py ~/bin/mvn-parse-pom
 	cp mvn-upload-artifact.sh ~/bin/mvn-upload-artifact
+	dos2unix ~/bin/mvn-*
 
--- mvn-parse-pom.py
+++ mvn-parse-pom.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
 import xml.etree.ElementTree
 
@@ -7,18 +8,32 @@ MAVEN_NS = { 'maven': 'http://maven.apache.org/POM/4.0.0' }
 
 def parse(pom_file, format):
     root = xml.etree.ElementTree.parse(pom_file).getroot()
-    def text(name, default=None):
+    def text(name, search_parent=True, default=None):
         elements = root.findall('maven:%s' % name, MAVEN_NS)
+        if not elements:
+            elements = root.findall(name)
+        if not elements and search_parent:
+            parent = root.findall('maven:parent', MAVEN_NS)
+            if not parent:
+                parent = root.findall('parent')
+            if parent:
+                if len(parent) > 1:
+                    raise Exception('more than one element found: parent')
+                elements = parent[0].findall('maven:%s' % name, MAVEN_NS)
+                if not elements:
+                    elements = parent[0].findall(name)
         if elements:
             if len(elements) > 1:
                 raise Exception('more than one element found: %s' % name)
             return elements[0].text
+        if not default:
+            raise Exception('element not found: %s' % name)
         return default
     groupId = text('groupId')
     groupPath = groupId.replace('.', '/')
     artifactId = text('artifactId')
     version = text('version')
-    packaging = text('packaging', 'jar')
+    packaging = text('packaging', False, 'jar')
     if format == 'gav':
         print('%s:%s:%s' % (groupId, artifactId, version))
     elif format == 'artifact':
@@ -34,8 +49,13 @@ def main():
     parser.add_argument('-F', '--format', metavar='<format>', default='gav',
             help='Output format: gav, artifact or path')
     args = parser.parse_args()
+    exit_code = 0
     for pom_file in args.pom:
-        parse(pom_file, args.format)
+        try:
+            parse(pom_file, args.format)
+        except Exception as e:
+            print('error parsing %s: %s' % (pom_file, e), file=sys.stderr)
+    sys.exit(exit_code)
 
 if __name__ == '__main__':
     main()
